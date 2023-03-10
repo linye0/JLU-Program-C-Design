c1df75d6f3d9a5abfbc10e488ba175d9dbcf299f
Zachary_chn <3106440285@qq.com>
2023-03-10 20:24:25 +0800

    update linye's .h file

--- beverageManagementSystem/merchantAndClient.h
+++ beverageManagementSystem/merchantAndClient.h
@@ -1,15 +1,16 @@
 #ifndef MERCHANTANDCLIENT_H
 #define MERCHANTANDCLIENT_H
-
 typedef struct beverageLinkedList {
 
     char brand[10];
 
     char name[100];
 
-    int time;
+    char time[100];
 
     int storeNum;
+
+    int price;
 
     char info[1000];
 
@@ -17,7 +18,8 @@ typedef struct beverageLinkedList {
 
 }*pBeverageList, BeverageList;
 
-typedef pBeverageList beverageNode;
+typedef BeverageList BeverageNode;
+typedef pBeverageList pBeverageNode;
 
 typedef struct beverageData {
 
@@ -29,29 +31,48 @@ typedef struct beverageData {
 
     int storeNum; // 存量
 
+    int price; // 价格
+
     char info[1000]; // 酒水信息
 
 }*pBeverageData, BeverageData;
 
 int isEmpty(pBeverageList list); // 判断链表是否为空，返回int类型真值
 
-void add(pBeverageList list, beverageNode newNode); // 往list中加入newNode
+void add(pBeverageList list, pBeverageNode newNode); // 往list中加入newNode
 
 pBeverageList init(); // 初始化，创建空链表
 
-pBeverageList createFromString(char* record); // 从字符串读入进货记录，存入链表中
+pBeverageNode find(pBeverageList head, int i); // 找到数据库中编号为i的酒水，find函数的编号是从0开始的，而不是从1开始
 
-// pBeverageList createFromFile(File* fp); // 从文件读入进货记录，存入链表中
+pBeverageNode insert(pBeverageList list, pBeverageNode node, int i); // 把node插入到list里编号为i的结点前
 
-pBeverageList sortBeverage(pBeverageList list, char* key); // 根据关键词对链表进行排序
+pBeverageNode newBeverageNode(char brand[], char name[], char time[], int storeNum, int price, char info[]); // 根据参数创建一个新的酒水结点
 
-void printStaff(pBeverageList list); // 打印链表，打印时酒水前面会显示从0开始的编号，供后续操作参考
+pBeverageList createFromFile(char* file); // 从文件读入进货记录，存入链表中
 
-void deleteBeverage(int number); // 根据编号删除指定结点
+void showStaff(pBeverageList list); // 打印链表，打印时酒水前面会显示从0开始的编号，供后续操作参考
 
-pBeverageList chageBeverage(int number, char* key, char* newValue); // 修改酒水属性
+pBeverageList sortBeverageTime(pBeverageList list, int key); // 对酒水以进货时间为关键词进行排序
 
-beverageNode searchBeverage(struct beverageData data); // 还没想清楚search的关键字要怎么搞
+pBeverageList sortBeverageBrand(pBeverageList list, int key); // 对酒水以品牌为关键词进行排序
+
+pBeverageList sortBeverageStoreNum(pBeverageList list, int key); // 对酒水以库存量为关键词进行排序
+
+pBeverageList sortBeveragePrice(pBeverageList list, int key); // 对酒水以价格为关键词进行排序
+
+void deleteBeverage(pBeverageList list, int pos); // 根据编号删除指定结点
+
+void changeBeverageInfo(pBeverageList list, int number, char* givenInfo); // 修改编号为number的酒水信息为info
+
+void reduceBeverageStoreNum(pBeverageList list, int number, int reduceNum); // 减少酒水的库存
+
+void searchBeverageBrand(pBeverageList list, char* givenBrand); // 根据givenBrand查询对酒水的品牌进行查询
+
+void searchBeverageInfo(pBeverageList list, char* givenInfo); // 根据givenInfo对酒水的信息进行查询
+
+void searchBeverageName(pBeverageList list, char* givenName); // 根据givenName对酒水的名称进行查询
+
 //************************************下面都是我的捏******************************************************
 
 typedef struct clientLinkedList {
